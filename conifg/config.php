2d3611a990a029a1d73d2a1a2025e29349bc40d8
Jeremy Cox <jer@supertechguy.com>
2024-03-15 12:58:38 -0600

    Added config file

---
+++ conifg/config.php
@@ -0,0 +1,15 @@
+<?php
+
+////////////////////
+///CONFIG
+////////////////////
+
+$mysql_host="localhost";
+$mysql_user="passwdtrade";
+$mysql_passwd="password";
+$mysql_db="passwdtrade";
+
+$project_title="passwd.trade | secure password transfer";
+$project_url="https://www.passwd.trade";
+
+?>
